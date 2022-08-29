83cfe803c6efb6cefdef5e5505909f26190ffae0
33394 <74552924+33394@users.noreply.github.com>
2022-08-29 14:48:27 +0800

    优化 2022/8/29/14

--- src/components/hotspot/DynamicCard.vue
+++ src/components/hotspot/DynamicCard.vue
@@ -1,11 +1,19 @@
 <template>
-  <div  class="dynamic-card background-common">
-    <div class="flex" style="align-items: flex-start">
+  <div class="dynamic-card">
+    <div class="flex background-common">
       <avatar/>
+      <div id="dynamic-content">
+        <span>{{ '小刺猬' }}</span>
+        <span style="color:#909090;">{{ '前端小刺猬' + "25分钟前" }}</span>
+        <span>{{ '动态内容' }}</span>
+        <div class="hot-reviews" v-show="true">
+
+          评论内容
+        </div>
+      </div>
     </div>
-    <h1>聊天卡片</h1>
     <el-divider/>
-    <ul class="flex-center-h100 space-around" style="margin: 0; padding: 10px;">
+    <ul class="flex-center-h100 background-common space-around" >
       <li>
         <img src="../../assets/image/share.png" alt="分享">
         <span>分享</span>
@@ -25,6 +33,7 @@
 
 <script>
 import avatar from '@/components/avatar/Avatar.vue'
+
 export default {
   name: "DynamicCard",
   components: {
@@ -33,28 +42,50 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+@import "src/assets/css/root";
 .dynamic-card {
-  background-color: #fff;
-  margin: 0 0 20px 0;
-  padding-bottom: 0;
-}
-.dynamic-card img {
-  width: 16px;
-  height: 16px;
-  padding-right: 4px;
-}
+  margin-bottom: 10px;
+  font-size: 15px;
+  color: #17181a;
+  img {
+    width: 16px;
+    height: 16px;
+    padding-right: 4px;
+  }
 
-.dynamic-card ul li {
-  display: flex;
-  align-items: center;
-  font-size: 10px;
-  color: #86909c;
+  .hot-reviews {
+    background: #f7f8fa;
+    border-radius: 4px;
+    padding: 12px 16px;
+  }
 
+  #dynamic-content {
+    margin-left: 10px;
+    width: 100%;
+    text-align: left;
+    @extend .flex-column;
+    @extend .space-between;
+  }
+
+  ul {
+
+    margin: 0;
+    padding: 10px 10px 0 10px;
+    li {
+      display: flex;
+      align-items: center;
+      font-size: 10px;
+      color: #86909c;
+    }
+
+
+  }
 }
 
 .el-divider--horizontal {
   margin: 0;
 }
+
+
 </style>
